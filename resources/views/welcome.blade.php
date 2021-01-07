5ac57a5fe270f6d8a396502087d3a81712aa5b6a
kubulu <kubulu@tecmavin.com>
2021-01-07 17:56:32 +0530

    initial push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,279 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <title>Sahasra Grand</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<body>
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+<div class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="logo">
+                    <img src="images/logo.png" />
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            </div>
+            <div class="col-6 d-flex align-items-center justify-content-end">
+                <div class="contact">
+                    <div class="image">
+                        <img src="images/call.png"/>
+                    </div>
+                    <div class="contact-content">
+                        <h5>Call for any Assistance</h5>
+                        <h3>+91 90190 23457</h3>
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="user">
+                    <div class="user-image">
+                        <img src="images/user.png" />
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</div>
+
+<div class="main-container">
+    <div class="container">
+        <div class="container-block">
+            <div class="row">
+                <div class="col-12">
+                    <div class="title text-center">
+                        <h2>Welcome to <span>Saharsa Grand</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="info-card-block">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 col-lg-4">
+                        <div class="info-card active">
+                            <div class="image"><img src="images/login.png"/> </div>
+                            <div class="button">
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal" class="btn btn-primary w-100">Login</a>
+                            </div>
+                            <div class="button">
+                                <a href="#" data-bs-toggle="modal" data-bs-target="#registerModal"  class="btn btn-secondary w-100">Create an Account</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-lg-4">
+                        <div class="info-card">
+                            <div class="image"><img src="images/icon-1.png"/> </div>
+                            <div class="button">
+                                <h3>Badminton</h3>
+                            </div>
+                            <div class="button">
+                                <a href="#" class="btn btn-secondary w-100">Enroll</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-lg-4">
+                        <div class="info-card ">
+                            <div class="image"><img src="images/icon-3.png"/> </div>
+                            <div class="button">
+                                <h3>Swimming</h3>
+                                <span>Coming Soon</span>
+                            </div>
+                            <div class="button">
+                                <a href="#" class="btn btn-secondary w-100">Enroll</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-lg-4">
+                        <div class="info-card ">
+                            <div class="image"><img src="images/icon-4.png"/> </div>
+                            <div class="button">
+                                <h3>Gym</h3>
+                                <span>Coming Soon</span>
+                            </div>
+                            <div class="button">
+                                <a href="#" class="btn btn-secondary w-100">Enroll</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-lg-4">
+                        <div class="info-card ">
+                            <div class="image"><img src="images/icon-5.png"/> </div>
+                            <div class="button">
+                                <h3>Snooker</h3>
+                                <span>Coming Soon</span>
+                            </div>
+                            <div class="button">
+                                <a href="#" class="btn btn-secondary w-100">Enroll</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-lg-4">
+                        <div class="info-card ">
+                            <div class="image"><img src="images/icon-6.png"/> </div>
+                            <div class="button">
+                                <h3>Table Tennis</h3>
+                                <span>Coming Soon</span>
+                            </div>
+                            <div class="button">
+                                <a href="#" class="btn btn-secondary w-100">Enroll</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-lg-4">
+                        <div class="info-card ">
+                            <div class="image"><img src="images/icon-7.png"/> </div>
+                            <div class="button">
+                                <h3>Party Hall</h3>
+                                <span>Coming Soon</span>
+                            </div>
+                            <div class="button">
+                                <a href="#" class="btn btn-secondary w-100">Book</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-lg-4">
+                        <div class="info-card ">
+                            <div class="image"><img src="images/icon-8.png"/> </div>
+                            <div class="button">
+                                <h3>Maintenance Payment</h3>
+                                <span>Coming Soon</span>
+                            </div>
+                            <div class="button">
+                                <a href="#" class="btn btn-secondary w-100">Pay Now</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-lg-4">
+                        <div class="info-card ">
+                            <div class="image"><img src="images/icon-9.png"/> </div>
+                            <div class="button">
+                                <h3>Grievance</h3>
+                                <span>Coming Soon</span>
+                            </div>
+                            <div class="button">
+                                <a href="#" class="btn btn-secondary w-100">Submit</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <p>Copyright © 2021 Sahasra Grand</p>
+                <p>Designed and Developed by NetiApps</p>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal" tabindex="-1" id="loginModal">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Login</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1">
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Remember</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal" tabindex="-1" id="registerModal">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Create an Account</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <form>
+                            <div class="mb-3">
+                                <label class="form-label">Full Name*</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Mobile Number*</label>
+                                <input type="number" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Email Address*</label>
+                                <input type="number" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Block*</label>
+                                <input type="number" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Flat Number*</label>
+                                <input type="number" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Type*</label>
+                                <input type="number" class="form-control">
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<script type="application/javascript" src="js/bootstrap.js"> </script>
+
+
+</body>
 </html>
