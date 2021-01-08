f89c08467da1be18c26845a731e6688e568a8e9b
kubulu <kubulu@tecmavin.com>
2021-01-08 15:36:18 +0530

    initial push

--- resources/views/booking/badminton.blade.php
+++ resources/views/booking/badminton.blade.php
@@ -13,7 +13,46 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div id="calendar"></div>
+                       <div class="col-md-6"></div>
+                        <div class="col-md-6">
+                            <div class="act-cont">
+                                <div class="user-details">
+                                    <div class="user-name"><h4>Janakiraman</h4></div>
+                                    <div class="user-name"><h6>Flat No. 1232</h6></div>
+                                </div>
+                                <div class="activity-container">
+                                    <div class="custom-control custom-radio custom-control-inline">
+
+                                        <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                                        <label class="custom-control-label" for="customRadioInline1">Family Members</label>
+
+                                        <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input ml-4">
+                                        <label class="custom-control-label" for="customRadioInline2">Apartment Members</label>
+                                    </div>
+                                </div>
+                                <div class="activity-member">
+                                    <h4>Add Members Details</h4>
+                                    <p>Min 2 and Max 6 Players are allowed per Slot</p>
+
+                                    <div class="members">
+                                        <div class="input-group pb-4">
+                                            <input type="text" class="form-control" placeholder="Player Name" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                            <div class="input-group-append">
+                                                <button class="btn btn-outline-secondary" type="button">Remove</button>
+                                                <button class="btn btn-outline-secondary" type="button">Add More</button>
+                                            </div>
+                                        </div>
+                                        <div class="input-group pb-4">
+                                            <input type="text" class="form-control" placeholder="Player Name" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                            <div class="input-group-append">
+                                                <button class="btn btn-outline-secondary" type="button">Remove</button>
+                                                <button class="btn btn-outline-secondary" type="button">Add More</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
